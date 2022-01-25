28610aa6c30f15e674858fc6a3a3aaa548c8e36a
Shahriyar Rzayev <rzayev.shahriyar@yandex.com>
2022-01-25 07:01:31 +0000

    Added optional packages and pre-commit hook for auto-formatting of the source code (#35)
    * added pre-commit
    
    * changed pre-commit
    
    * added formatter pre-commit
    
    Co-authored-by: shako <shako@caios.io>

--- src/dynacli.py
+++ src/dynacli.py
@@ -13,8 +13,19 @@ from itertools import product
 from os import path
 from pkgutil import iter_modules
 from types import MappingProxyType, ModuleType
-from typing import (Any, AnyStr, Callable, Final, Iterator, Match, Optional,
-                    Pattern, Type, TypedDict, Union)
+from typing import (
+    Any,
+    AnyStr,
+    Callable,
+    Final,
+    Iterator,
+    Match,
+    Optional,
+    Pattern,
+    Type,
+    TypedDict,
+    Union,
+)
 
 # This is for indicating the version of our CLI
 __version__: Final[str] = "1.0"
