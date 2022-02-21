7c5cf2ee6c282d32cb1ddbd6d748440dcd14a52a
shako <shako@caios.io>
2022-02-21 07:54:56 +0000

    supporting keyword arguments as part of init

--- src/dynacli/bootstrap/init.py
+++ src/dynacli/bootstrap/init.py
@@ -3,22 +3,23 @@ import shutil
 import stat
 
 
-def init(name: str, path: str) -> None:
+def init(name: str, **kwargs: str) -> None:
     """
     Initialize the CLI entrypoint in given path
 
     Args:
         name (str): name of the new CLI tool
-        path (str): The path for CLI entrypoint
+        **kwargs (str): The keyword arguments for CLI entrypoint
 
     Return: None
     """
-    wd = os.path.dirname(os.path.realpath(__file__))
-    cwd = path if path != "." else os.path.curdir
+    _wd = os.path.dirname(os.path.realpath(__file__))
+    _path = kwargs.get("path")
+    _cwd = _path if _path and _path != "." else os.path.curdir
     try:
-        shutil.copyfile(f"{wd}/_sample_cli", f"{cwd}/{name}")
-        os.chmod(f"{cwd}/{name}", stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+        shutil.copyfile(f"{_wd}/_sample_cli", f"{_cwd}/{name}")
+        os.chmod(f"{_cwd}/{name}", stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
     except Exception as err:
         print("Failed to create CLI entrypoint with ", str(err))
     else:
-        print(f"Successfully created CLI entrypoint {name} at {cwd}")
+        print(f"Successfully created CLI entrypoint {name} at {os.path.abspath(_cwd)}")
