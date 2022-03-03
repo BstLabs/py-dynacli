3c8c1964777d6389937b4717cb1f59cb77e03fa6
shako <shako@caios.io>
2022-03-03 08:12:33 +0000

    Fixed the missing description and version in dynacli entrypoint

--- src/dynacli/bootstrap/_cli.py
+++ src/dynacli/bootstrap/_cli.py
@@ -7,6 +7,7 @@ DynaCLI bootstrap script
 import os
 import sys
 
+from dynacli import __version__ as _version
 from dynacli import main as dynamain
 
 cwd = os.path.dirname(os.path.realpath(__file__))
@@ -15,8 +16,25 @@ search_path = [cwd]
 sys.path.extend(search_path)
 
 
+# This fix is for dynacli entrypoint script; as it has wrapper __main__ we need to add necessary information
+
+_map = {
+    "__version__": _version,
+    "__doc__": """
+DynaCLI bootstrap script
+""",
+}
+
+
+def _set_main_attrs(**kwargs):
+    _main = sys.modules["__main__"]
+    for key, val in kwargs.items():
+        setattr(_main, key, val)
+
+
 # For package distro purposes
 def main():
+    _set_main_attrs(**_map)
     dynamain(search_path)
 
 
